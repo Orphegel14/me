f354c6810edd90d8536031edb96f57c584de4ea8
Ross Ching <z5355017@ad.unsw.edu.au>
2023-06-05 07:50:26 +1000

    3

--- set1/exercise1.py
+++ set1/exercise1.py
@@ -1,12 +1,5 @@
 """Your very first python program!
 
 TODO: write a python script that prints "Hello world!"
-
-Look on the course website for how to do this, and for how to run the tests.
-
-https://design-computing.github.io/md/week1
-
 """
-# 👇👇 Write your code here 👇👇
-print("HELLO WORLD!!")
-# 👆👆 Write your code here 👆👆
+print("HELLO WORLD")
